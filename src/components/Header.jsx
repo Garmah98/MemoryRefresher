2db4927256b3add443426541a64f880b28a36a02
Garmah <PiotrekG000@gmail.com>
2024-10-30 05:10:29 +0100

    style: styled header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
 	const { createCard } = useContext(ProgressContext);
 	return (
 		<header className='p-3 w-full bg-first flex justify-between'>
-			<h1 className='text-3xl text-third'>MemoryRefresher</h1>
+			<h1 className='text-3xl mr-1 text-third'>MemoryRefresher</h1>
 			<Button className='w-32' onClick={createCard}>
 				Create Card
 			</Button>
