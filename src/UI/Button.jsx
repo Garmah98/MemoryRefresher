6ba79a8432c8aa145698ca1f6eb07d8fcb5e8a04
Garmah <PiotrekG000@gmail.com>
2024-10-30 04:37:57 +0100

    style: added button on hover effect

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -1,10 +1,10 @@
 export default function Button({ children, className, textOnly, ...props }) {
 	let stylingClasses =
-		'p-2 rounded-md text-first text-lg bg-third shadow-md font-semibold ';
+		'p-2 rounded-md text-first text-lg bg-third shadow-md font-semibold hover:scale-95 ';
 	stylingClasses += className;
 	if (textOnly) {
 		stylingClasses =
-			'm-1 p-2 text-first text-lg font-semibold border border-third rounded-md shadow-md ';
+			'm-1 p-2 text-first text-lg font-semibold border border-third rounded-md shadow-md hover:scale-95 ';
 		stylingClasses += className;
 	}
 	return (
