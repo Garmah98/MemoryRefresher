6871217902185d2e26b868938f2302b9eecd79d8
Garmah <PiotrekG000@gmail.com>
2024-11-04 12:05:27 +0100

    style:hold to remove button now fills up to indicate removal progress

--- src/UI/HoldButton.jsx
+++ src/UI/HoldButton.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useContext } from 'react';
 import { CardContext } from '../context/CardContext';
 import { ProgressContext } from '../context/ProgressContext';
@@ -6,6 +6,7 @@ import { ProgressContext } from '../context/ProgressContext';
 const DELAY = 2000;
 
 export default function HoldButton({ children, className, ...props }) {
+	const [progress, setProgress] = useState(0);
 	const { removeCard } = useContext(CardContext);
 	const { hideModal } = useContext(ProgressContext);
 	const startTime = useRef(null);
@@ -19,6 +20,7 @@ export default function HoldButton({ children, className, ...props }) {
 	function startTimer() {
 		startTime.current = Date.now();
 		holdRef.current = setInterval(() => {
+			setProgress(Math.floor(((Date.now() - startTime.current) / DELAY) * 100));
 			if (startTime.current && Date.now() - startTime.current > DELAY) {
 				handleRemove();
 				stopTimer();
@@ -27,14 +29,14 @@ export default function HoldButton({ children, className, ...props }) {
 	}
 	function stopTimer() {
 		startTime.current = null;
+		setProgress(0);
 		if (holdRef.current) {
 			clearInterval(holdRef.current);
 			holdRef.current = null;
 		}
 	}
 
-	let stylingClasses =
-		'm-1 p-2 text-first text-lg font-semibold border border-third rounded-md shadow-md hover:scale-95 ';
+	let stylingClasses = `m-1 p-2 text-first text-lg font-semibold border border-third rounded-md shadow-md hover:scale-95 bg-gradient-to-r from-black/15 to-black/15 bg-no-repeat `;
 	stylingClasses += className;
 
 	return (
@@ -44,6 +46,7 @@ export default function HoldButton({ children, className, ...props }) {
 			onMouseLeave={stopTimer}
 			{...props}
 			className={stylingClasses}
+			style={{ backgroundSize: `${progress}%` }}
 		>
 			{children}
 		</button>
