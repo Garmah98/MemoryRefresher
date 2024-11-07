2ccd12a5b5b336010a2291f812ecb144a79511b8
Garmah <PiotrekG000@gmail.com>
2024-11-07 13:44:02 +0100

    style: fixed horizontal scroll

--- src/components/MainBoard.jsx
+++ src/components/MainBoard.jsx
@@ -9,7 +9,7 @@ export default function MainBoard() {
 
 
 	return (
-		<main className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4'>
+		<main className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 m-1 '>
 			{items.map((item) => {
 				return (
 					<Card
