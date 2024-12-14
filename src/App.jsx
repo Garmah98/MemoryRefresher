33265f880ae5d72e8131c927417746ad722a6b3b
Garmah <PiotrekG000@gmail.com>
2024-12-14 14:44:11 +0100

    'chore:fixed typo'

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import Header from './components/Header';
-import MainBoard from './components/MainBoard';
-import ProgressContextProvider from './context/ProgressContext';
-import CardContextProvider from './context/CardContext';
-import CardModal from './components/CardModal';
-import CreateCard from './components/createCard';
+import Header from './components/Header'
+import MainBoard from './components/MainBoard'
+import ProgressContextProvider from './context/ProgressContext'
+import CardContextProvider from './context/CardContext'
+import CardModal from './components/CardModal'
+import CreateCard from './components/CreateCard'
 
 function App() {
-	return (
-		<CardContextProvider>
-			<ProgressContextProvider>
-				<CreateCard />
-				<CardModal />
-				<Header />
-				<MainBoard />
-			</ProgressContextProvider>
-		</CardContextProvider>
-	);
+    return (
+        <CardContextProvider>
+            <ProgressContextProvider>
+                <CreateCard />
+                <CardModal />
+                <Header />
+                <MainBoard />
+            </ProgressContextProvider>
+        </CardContextProvider>
+    )
 }
 
-export default App;
+export default App
