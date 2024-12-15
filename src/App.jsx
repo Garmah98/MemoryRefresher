773e833fe19ca045fa223ca8cf5bdafa924b87df
Garmah <PiotrekG000@gmail.com>
2024-12-15 14:10:54 +0100

    'refactor: added persisted state with redux'

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import CardModal from './components/CardModal'
 import CreateCard from './components/CreateCard'
 import { Provider } from 'react-redux'
 import store from './store/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import persistStore from 'redux-persist/es/persistStore'
+
+let persistor = persistStore(store)
 
 function App() {
     return (
         <Provider store={store}>
-            <CreateCard />
-            <CardModal />
-            <Header />
-            <MainBoard />
+            <PersistGate loading={null} persistor={persistor}>
+                <CreateCard />
+                <CardModal />
+                <Header />
+                <MainBoard />
+            </PersistGate>
         </Provider>
     )
 }
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,12 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import cardsReducer from './CardsSlice'
 import ProgressReducer from './ProgressSlice'
 
+const reducers = combineReducers({
+    cards: cardsReducer,
+    progress: ProgressReducer,
+})
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+const persistedReducer = persistReducer(persistConfig, reducers)
+
 const store = configureStore({
-    reducer: {
-        cards: cardsReducer,
-        progress: ProgressReducer,
-    },
+    reducer: persistedReducer,
 })
 
 export default store
